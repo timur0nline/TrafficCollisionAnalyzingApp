d2dcba67380368e4b66cc9f0fb8598247bc6a82d
timur0nline <fatihowt51@gmail.com>
2026-05-19 03:18:35 +0500

    Изменение полей

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -258,7 +258,7 @@
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(1260, 593);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Автомобили";
+            this.tabPage2.Text = "ТС";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // deleteVehicleButton
